8c43cf615759d5b18124085eb05da131e1f0f6a3
Phiremouse <Byford.Boyd@gmail.com>
2019-10-19 00:01:57 -0500

    started level 2

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,15 +4,12 @@ var tableData = data;
 // YOUR CODE HERE!
 var FilterBtn = d3.select('#filter-btn');
 var tbody = d3.select('tbody');
+var FilterField = d3.select('input');
 
-// todo: cant get this work 
-// var FilterField = d3.select('.form-control');
-
-//todo: capitalize city names, states, country
 //todo:transform special characters into symbols again
 
-
 function HandleChange(event){
+    tbody.text('');
     var InputElement= d3.select('#datetime');
     var InputValue = InputElement.property('value');
     var filterdata = tableData.filter(date => date.datetime===InputValue);   
@@ -20,12 +17,25 @@ function HandleChange(event){
         var row = tbody.append('tr');
         Object.entries(element).forEach(([key,value]) => {
             var cell = row.append('td');
-            cell.text(value);
+            if (key==='country'||key==='state'){
+                tempval=value.toUpperCase();
+                cell.text(tempval);
+            }
+            else{
+                MyString = value;
+                tempvalue=MyString[0].toUpperCase() + MyString.slice(1)
+                cell.text(tempvalue);
+            }
         });
     });    
 };
 
-FilterBtn.on("click",HandleChange);
+function MyEnter(event){
+    if(event.key===13){
+        HandleChange(event);
+    }
+}
 
-//todo: cant get this to work 
-// FilterField.on('change',HandleChange);
+FilterBtn.on("click",HandleChange);
+FilterField.on('change',HandleChange);
+FilterField.on('pressed',MyEnter);
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,44 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var FilterBtn = d3.select('#filter-btn');
+var tbody = d3.select('tbody');
+var FilterField = d3.select('input');
+
+//todo:transform special characters into symbols again
+
+function HandleChange(event){
+    tbody.text('');
+    var InputElement= d3.select('#datetime');
+    var InputValue = InputElement.property('value');
+    var filterdata = tableData.filter(date => date.datetime===InputValue);   
+    filterdata.forEach((element)=>{
+        var row = tbody.append('tr');
+        Object.entries(element).forEach(([key,value]) => {
+            var cell = row.append('td');
+            if (key==='country'||key==='state'){
+                tempval=value.toUpperCase();
+                cell.text(tempval);
+            }
+            else{
+                MyString = value;
+                tempvalue=MyString[0].toUpperCase() + MyString.slice(1)
+                cell.text(tempvalue);
+            }
+        });
+    });    
+};
+
+function MyEnter(event){
+    if(event.key===13){
+        HandleChange(event);
+    }
+}
+
+FilterBtn.on("click",HandleChange);
+FilterField.on('change',HandleChange);
+FilterField.on('pressed',MyEnter);
